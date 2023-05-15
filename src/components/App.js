0cdc7d6693574ead3be6dcc9d5ebbb5ab354f7f0
Govind <agovindchavanb@gmail.com>
2023-05-15 09:53:20 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[value,updateValue]=useState("");
+  function greet(e){
+    updateValue(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input 
+        onChange={greet}
+        /> 
+        {value} && 
+        <p>Hello {value}!</p>
     </div>
   )
 }
